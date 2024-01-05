6d3004a52f4fd444c4b37558486c55078d7d79b1
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-01-05 23:56:05 +0900

    add: 화면 공유하여 비디오 엘리먼트에 반영

--- nbc-nep-front/src/components/share-screen/ShareScreen.tsx
+++ nbc-nep-front/src/components/share-screen/ShareScreen.tsx
@@ -1,28 +1,50 @@
-import { useEffect, useRef } from "react";
+import { CSSProperties, useEffect, useRef } from "react";
 import { Socket, io } from "socket.io-client";
 
 export default function ShareScreen() {
   const socketRef = useRef<Socket | null>(null);
+  const myVideoRef = useRef<HTMLVideoElement | null>(null);
+  const otherVideoRef = useRef<HTMLVideoElement | null>(null);
 
   useEffect(() => {
-    if (!socketRef.current) {
-      socketRef.current = io("http://localhost:3001", {
-        withCredentials: true,
-      });
-      return;
-    }
+    socketRef.current = io("http://localhost:3001", {
+      withCredentials: true,
+    });
     const socket = socketRef.current;
-
-    if (socket.connected) return;
     socket.on("connect", () => {
       console.log("connected");
     });
     return () => {
-      if (socket && socket.connected) {
-        socket.disconnect();
-      }
+      socket.disconnect();
     };
   }, []);
 
-  return <div></div>;
+  const handleStartCapture = async () => {
+    const displayMediaOptions = {
+      video: {
+        displaySurface: "window",
+      },
+      audio: false,
+    };
+    if (!myVideoRef.current) return;
+
+    try {
+      myVideoRef.current.srcObject =
+        await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+    } catch (err) {
+      console.error("handle start capture", err);
+    }
+  };
+
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <button onClick={handleStartCapture}>Start Capture</button>
+      <video ref={myVideoRef} style={videoStyle} autoPlay></video>
+      <video ref={otherVideoRef} style={videoStyle} autoPlay></video>
+    </div>
+  );
 }
+
+const videoStyle: CSSProperties = {
+  border: "1px solid black",
+};
