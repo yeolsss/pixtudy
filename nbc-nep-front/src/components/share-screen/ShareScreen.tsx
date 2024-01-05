c2de55b5ff53b17d70d6fced3b5220f1498f9a32
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-01-06 00:30:44 +0900

    add: 두명의 사용자가 화면 공유할 수 있도록 기능 개발

--- nbc-nep-front/src/components/share-screen/ShareScreen.tsx
+++ nbc-nep-front/src/components/share-screen/ShareScreen.tsx
@@ -1,10 +1,47 @@
 import { CSSProperties, useEffect, useRef } from "react";
 import { Socket, io } from "socket.io-client";
-
+const configuration = {
+  iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+};
 export default function ShareScreen() {
   const socketRef = useRef<Socket | null>(null);
   const myVideoRef = useRef<HTMLVideoElement | null>(null);
   const otherVideoRef = useRef<HTMLVideoElement | null>(null);
+  const peerConnectionRef = useRef<RTCPeerConnection | null>(null);
+
+  useEffect(() => {
+    const peerConnection = new RTCPeerConnection(configuration);
+    peerConnectionRef.current = peerConnection;
+
+    const handleTrack = async (event: RTCTrackEvent) => {
+      const [remoteStream] = event.streams;
+      otherVideoRef.current!.srcObject = remoteStream;
+    };
+    const handleIceCandidate = (data: RTCPeerConnectionIceEvent) => {
+      socketRef.current!.emit("ice", data.candidate);
+    };
+    const handleConnectionChange = () => {
+      const peerConnection = peerConnectionRef.current!;
+      if (peerConnection.connectionState === "connected") {
+        console.log("connected");
+      }
+    };
+
+    peerConnection.addEventListener("track", handleTrack);
+    peerConnection.addEventListener("icecandidate", handleIceCandidate);
+    peerConnection.addEventListener(
+      "connectionstatechange",
+      handleConnectionChange
+    );
+    return () => {
+      peerConnection.removeEventListener("track", handleTrack);
+      peerConnection.removeEventListener("icecandidate", handleIceCandidate);
+      peerConnection.removeEventListener(
+        "connectionstatechange",
+        handleConnectionChange
+      );
+    };
+  }, []);
 
   useEffect(() => {
     socketRef.current = io("http://localhost:3001", {
@@ -14,6 +51,22 @@ export default function ShareScreen() {
     socket.on("connect", () => {
       console.log("connected");
     });
+    socket.on("offer", async (offer: RTCSessionDescription) => {
+      const peerConnection = peerConnectionRef.current!;
+      peerConnection.setRemoteDescription(offer);
+      const answer = await peerConnection.createAnswer();
+      peerConnection.setLocalDescription(new RTCSessionDescription(answer));
+      socket.emit("answer", answer);
+    });
+    socket.on("answer", async (answer: RTCSessionDescription) => {
+      const peerConnection = peerConnectionRef.current!;
+      await peerConnection.setRemoteDescription(answer);
+    });
+    socket.on("ice", async (iceCandidate: RTCIceCandidate) => {
+      const peerConnection = peerConnectionRef.current!;
+      await peerConnection.addIceCandidate(iceCandidate);
+    });
+
     return () => {
       socket.disconnect();
     };
@@ -29,12 +82,23 @@ export default function ShareScreen() {
     if (!myVideoRef.current) return;
 
     try {
-      myVideoRef.current.srcObject =
+      const mediaStream: MediaStream =
         await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+      myVideoRef.current.srcObject = mediaStream;
+      makeCall(mediaStream, peerConnectionRef.current!);
     } catch (err) {
       console.error("handle start capture", err);
     }
   };
+
+  async function makeCall(
+    mediaStream: MediaStream,
+    peerConnection: RTCPeerConnection
+  ) {
+    addTracksToConnection(mediaStream, peerConnection);
+    const offer = await createAndSetOffer(peerConnection);
+    socketRef.current!.emit("offer", offer);
+  }
 
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
@@ -48,3 +112,16 @@ export default function ShareScreen() {
 const videoStyle: CSSProperties = {
   border: "1px solid black",
 };
+function addTracksToConnection(
+  mediaStream: MediaStream,
+  peerConnection: RTCPeerConnection
+) {
+  mediaStream.getTracks().forEach((track) => {
+    peerConnection.addTrack(track, mediaStream);
+  });
+}
+async function createAndSetOffer(peerConnection: RTCPeerConnection) {
+  const offer = await peerConnection.createOffer();
+  await peerConnection.setLocalDescription(new RTCSessionDescription(offer));
+  return offer;
+}
