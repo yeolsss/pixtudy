9d7bd6ff7a89c68b05d28b6f30b51a6c22818d90
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-01-12 22:47:07 +0900

    [refactor] : ShareScreenButton.tsx 모듈화

--- nbc-nep-front/src/components/share-screen/multi/ShareScreenButton.tsx
+++ nbc-nep-front/src/components/share-screen/multi/ShareScreenButton.tsx
@@ -1,21 +1,33 @@
 interface Props {
   onShareScreen: (stream: MediaStream) => void;
+  mode: "screen" | "webcam";
 }
-const displayMediaOptions = {
-  video: {
-    displaySurface: "window",
-  },
-  audio: false,
-};
-export default function ShareScreenButton({ onShareScreen }: Props) {
+
+export default function ShareScreenButton({ onShareScreen, mode }: Props) {
   const handleStartCapture = async () => {
+    const isScreenMode = mode === "screen";
+
     try {
-      const mediaStream: MediaStream =
-        await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+      const mediaStream: MediaStream = await (isScreenMode
+        ? getDisplayMedia()
+        : getUserMedia());
+
       onShareScreen(mediaStream);
     } catch (err) {
       console.error("on error when start capture", err);
     }
   };
+
   return <button onClick={handleStartCapture}>Start Capture</button>;
 }
+
+const getDisplayMedia = () =>
+  navigator.mediaDevices.getDisplayMedia({
+    video: {
+      displaySurface: "window",
+    },
+    audio: false,
+  });
+
+const getUserMedia = () =>
+  navigator.mediaDevices.getUserMedia({ video: true, audio: true });
