5b19b34af65605622f997df90fd11c18f3104cbe
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-01-12 23:30:59 +0900

    [refactor] custom훅을 이용한 device 로직 분리

---
+++ nbc-nep-front/src/hooks/share-screen/useDevice.ts
@@ -0,0 +1,28 @@
+import {
+  RtpCapabilities,
+  ShareType,
+} from "@/components/share-screen/types/ScreenShare.types";
+import { Device, types } from "mediasoup-client";
+import { useEffect, useRef } from "react";
+export default function useDevice() {
+  const deviceRef = useRef<types.Device>();
+
+  useEffect(() => {
+    if (deviceRef.current) return;
+    deviceRef.current = new Device();
+  }, []);
+
+  async function loadDevice(rtpCapabilities: RtpCapabilities, type: ShareType) {
+    const device = deviceRef.current;
+    try {
+      await device?.load({ routerRtpCapabilities: rtpCapabilities });
+    } catch (error) {
+      console.error("load device error", error);
+    }
+  }
+
+  return {
+    device: deviceRef.current,
+    loadDevice,
+  };
+}
