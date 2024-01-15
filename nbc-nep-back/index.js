eae9d44fd828558dbfca6a322480b8a928c70116
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-01-15 10:27:45 +0900

    [fix]: 병합 과정에서 오류 나던거 수정

--- nbc-nep-back/index.js
+++ nbc-nep-back/index.js
@@ -2,7 +2,15 @@ const express = require("express");
 const http = require("http");
 const socketIO = require("socket.io");
 const cors = require("cors");
-const { handleJoinRoom, handleReceiveOffer } = require("./conference");
+const {
+  createWebRtcTransport,
+  createWorker,
+  getIsCanConsumeWithRouter,
+  getRtcCapabilities,
+  getTransportParams,
+} = require("./conference/mediaSoupManager.js");
+const gameServer = require("./gameServer");
+const chatServer = require("./chatServer");
 
 const app = express();
 let server = http.Server(app);
@@ -50,25 +58,266 @@ let transports = [];
 let peers = {};
 
 io.on("connection", (socket) => {
-  console.log("socket connected");
+  socket.on("join-room", ({ roomName, type }, setDevice) => {
+    console.log("socket on join-room");
+    // peers초기 세팅
+    peers[socket.id] = {
+      socket,
+      roomName,
+      transports: [],
+      producers: [],
+      consumers: [],
+      peerDetails: {
+        name: "",
+        isAdmin: false,
+      },
+    };
 
-  socket.on("join-room", handleJoinRoom(io));
-  socket.on("send-offer", handleReceiveOffer(io, socket));
-  socket.on("send-candidate-info");
+    const rtpCapabilities = getRtcCapabilities();
 
-  /* socket.on("offer", (offer) => {
-    socket.broadcast.emit("offer", offer);
+    setDevice(rtpCapabilities, type);
   });
-  socket.on("answer", (answer) => {
-    socket.broadcast.emit("answer", answer);
+
+  // device rtp capabilities를 받아서 세팅을 완료 했고 remote producer 혹은 consumer를 만드는 이벤트
+  socket.on(
+    "create-web-rtc-transport",
+    async ({ consumer, type }, callback) => {
+      if (!consumer) {
+        console.log(socket.id, " producer로서 createWebRtcTransport 호출");
+      } else {
+        console.log(socket.id, " consumer로서 createWebRtcTransport 호출");
+      }
+
+      const socketId = socket.id;
+      const roomName = peers[socket.id].roomName;
+
+      const { transport, params } = await createWebRtcTransport();
+
+      // transport를 등록한다. 이 transport는 producer이거나 consumer일 수 있다.
+      transports = [...transports, { socketId, roomName, transport, consumer }];
+
+      // 사용자의 peer에 transport를 추가한다.
+      setPeers(socketId, "transports", (prevTransports) => [
+        ...prevTransports,
+        transport.id,
+      ]);
+
+      console.log("create web rtc transport success");
+
+      if (consumer) {
+        // consumer일 경우에는 다른 로직을 작성해야 하기 때문에 분기 처리
+        callback({ params });
+      } else {
+        console.log("socket emit created web-rtc-transport");
+        socket.emit("created-web-rtc-transport", params, type);
+      }
+    }
+  );
+
+  // 서버측 transport와 클라이언트 측 transport를 연결(핸드쉐이크)하는 이벤트
+  socket.on("transport-connect", ({ dtlsParameters }) => {
+    console.log("start - socket on 'transport connect' ");
+
+    const transport = getTransportProduce(socket.id);
+
+    if (!transport) {
+      console.log("no transport found");
+      return;
+    }
+    try {
+      transport.connect({ dtlsParameters });
+      console.log("end - 'transport connect' success");
+    } catch (error) {
+      console.log("oops, transport connect error", error);
+    }
   });
-  socket.on("ice", (ice) => {
-    socket.broadcast.emit("ice", ice);
-  }); */
+
+  // 클라이언트에서 produce했을 때 서버에서 받는 이벤트 핸들러
+  // 클1이 produce -> 서버(remote producer)
+  socket.on(
+    "transport-produce",
+    async ({ kind, rtpParameters, appData, socketId }, callback) => {
+      console.log("start - socket on 'transport produce'");
+      //TODO : kind에 따른 처리를 해줘야 한다. (video 인지 audio 인지)
+      console.log("app Data is : ", appData);
+      const transport = getTransportProduce(socketId);
+
+      try {
+        const remoteProducer = await transport.produce({
+          kind,
+          rtpParameters,
+          appData,
+        });
+
+        const { roomName } = peers[socket.id];
+        console.log(`${socketId}의 remote producer id is ${remoteProducer.id}`);
+        addProducer(remoteProducer, roomName, socket.id, socket.name);
+
+        setPeers(socket.id, "producers", (prevProducers) => [
+          ...prevProducers,
+          remoteProducer.id,
+        ]);
+
+        informToConsumers(roomName, socket.id, remoteProducer.id);
+
+        remoteProducer.on("transportclose", () => {
+          remoteProducer.close();
+        });
+
+        console.log("end - 'transport produce' success");
+        const producersExist = getExistsProducers(roomName, socket.id);
+
+        callback({
+          id: remoteProducer.id,
+          producersExist,
+        });
+      } catch (error) {
+        console.log("oops, transport produce error", error);
+      }
+    }
+  );
+
+  socket.on("get-producers", (callback) => {
+    const { roomName } = peers[socket.id];
+    // ? 이 값은 왜 필요한거지?
+    const socketName = peers[socket.id].peerDetails.name;
+
+    let producerList = producers
+      .filter(
+        (producer) =>
+          producer.roomName === roomName && producer.socketId !== socket.id
+      )
+      .map((producer) => ({
+        producerId: producer.producer.id,
+        socketName: peers[producer.socketId].peerDetails.name,
+        socketId: producer.socketId,
+        isNewSocketHost: peers[producer.socketId].peerDetails.isAdmin,
+      }));
+
+    callback(producerList);
+  });
+
+  socket.on(
+    "consume",
+    async (
+      { rtpCapabilities, remoteProducerId, serverConsumerTransportId },
+      callback
+    ) => {
+      const { roomName } = peers[socket.id];
+      const userName = peers[socket.id].peerDetails.name;
+
+      // 서버 transport중 내가 consume할 trasnport를 찾는다
+      const consumerTransport = transports.find(
+        (transport) =>
+          transport.consumer &&
+          transport.transport.id === serverConsumerTransportId
+      ).transport;
+
+      if (
+        !getIsCanConsumeWithRouter({
+          producerId: remoteProducerId,
+          rtpCapabilities,
+        })
+      ) {
+        console.log("can not consume");
+        return;
+      }
+
+      try {
+        console.log("start - consume");
+        console.log(
+          `${socket.id}가 컴슘을 하는데 remote producer id is ${remoteProducerId}`
+        );
+        const consumer = await consumerTransport.consume({
+          producerId: remoteProducerId,
+          rtpCapabilities,
+          paused: true,
+        });
+
+        consumer.on("transportclose", () => {
+          console.log("transport close from consumer");
+        });
+
+        consumer.on("producerclose", () => {
+          socket.emit("producer-closed", { remoteProducerId });
+
+          consumerTransport.close();
+
+          consumers = consumers.filter(
+            (otherConsumer) => otherConsumer.consumer.id !== consumer.id
+          );
+        });
+
+        addConsumer(consumer, roomName, socket.id);
+
+        setPeers(socket.id, "consumers", (prevConsumers) => [
+          ...prevConsumers,
+          consumer.id,
+        ]);
+
+        const params = {
+          id: consumer.id,
+          producerId: remoteProducerId,
+          kind: consumer.kind,
+          rtpParameters: consumer.rtpParameters,
+          serverConsumerId: consumer.id,
+          userName,
+        };
+
+        console.log("end- consume");
+
+        callback(params);
+      } catch (error) {
+        console.log("oops consume error", error);
+      }
+    }
+  ); //
+
+  socket.on(
+    "transport-recv-connect",
+    async ({ dtlsParameters, serverConsumerTransportId }) => {
+      console.log('start - socket on "transport-recv-connect"');
+      const consumerTransport = transports.find(
+        (transport) =>
+          transport.transport.id === serverConsumerTransportId &&
+          transport.consumer
+      )?.transport;
+
+      if (!consumerTransport) {
+        console.log("oops, no transport found");
+        return;
+      }
+
+      try {
+        await consumerTransport.connect({ dtlsParameters });
+        console.log("end - socket on 'transport-recv-connect' success");
+      } catch (error) {
+        console.log("transport-recv-connect error", error);
+      }
+    }
+  );
+
+  socket.on("consumer-resume", async ({ serverConsumerId }) => {
+    console.log("start - socket on 'consumer-resume'");
+    const consumer = consumers.find(
+      (consumer) => consumer.consumer.id === serverConsumerId
+    )?.consumer;
+    if (consumer) {
+      await consumer.resume();
+      console.log("end - socket on 'consumer-resume' success");
+    }
+  });
 });
 
+const metaverseNamespace = io.of("/metaverse");
+gameServer(metaverseNamespace);
+
+// chat server Socket Handlers
+const chatNamespace = io.of("/chat");
+chatServer(chatNamespace);
+
 server.listen(3001, () => {
-  console.log("Server listening on port 3000");
+  console.log("Server listening on port 3001");
 });
 
 function getTransportProduce(socketId) {
