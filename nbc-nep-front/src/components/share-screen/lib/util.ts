8dc37d219ae8d5ab1c2ae0fa94186769fc873262
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-01-15 23:47:21 +0900

    [chores] : 불필요 파일 제거

--- nbc-nep-front/src/components/share-screen/lib/util.ts
+++ nbc-nep-front/src/components/share-screen/lib/util.ts
@@ -1,15 +1,4 @@
 import { Consumer } from "mediasoup-client/lib/Consumer";
-import { ConsumerTransportType } from "../types/ScreenShare.types";
-
-// ! 이번것이 해결된다면 사라져야 마땅함
-export function isAlreadyConsumeTransport(
-  consumerTransports: ConsumerTransportType[],
-  remoteProducerId: string
-) {
-  return consumerTransports.some((consumerTransport) => {
-    return consumerTransport.producerId === remoteProducerId;
-  });
-}
 
 export function isAlreadyConsume(
   consumers: Consumer[],
@@ -18,20 +7,10 @@ export function isAlreadyConsume(
   return consumers.some((consumer) => consumer.id === remoteProducerId);
 }
 
-export function checkStreamTracksEmpty(stream: MediaStream) {
-  return (
-    stream.getAudioTracks().length === 0 && stream.getVideoTracks().length === 0
-  );
-}
-
 export function isEmptyTracks(tracks: MediaStreamTrack[]) {
   return tracks.length === 0;
 }
 
 export function isVideoTrack(track: MediaStreamTrack) {
   return track.kind === "video";
-}
-
-export function isAudioTrack(track: MediaStreamTrack) {
-  return track.kind === "audio";
 }
