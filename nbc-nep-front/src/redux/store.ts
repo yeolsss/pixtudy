fa17dffc804d3df492d14d08a34bdca529d56a67
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-01-17 23:51:23 +0900

    [update] : redux 적용

--- nbc-nep-front/src/redux/store.ts
+++ nbc-nep-front/src/redux/store.ts
@@ -1,13 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { createWrapper } from "next-redux-wrapper";
 import authSlice from "./modules/authSlice";
-import modalSlice from "./modules/modalSlice";
-import globalNavBar from "./modules/globalNavBarSlice";
 import chatType from "./modules/chatTypeSlice";
+import conferenceSlice from "./modules/conferenceSlice";
+import globalNavBar from "./modules/globalNavBarSlice";
+import modalSlice from "./modules/modalSlice";
 
 export const makeStore = () => {
   return configureStore({
-    reducer: { modalSlice, authSlice, globalNavBar, chatType },
+    reducer: { modalSlice, authSlice, globalNavBar, chatType, conferenceSlice },
   });
 };
 
