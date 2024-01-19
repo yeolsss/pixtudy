4ebe6450cbb4dfefb1028ac46d1cff9d449637b3
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-01-19 19:21:41 +0900

    [refactor] : 매직넘버 및 상수값 상수파일 만듬

---
+++ nbc-nep-front/src/components/video-conference/constants/constants.ts
@@ -0,0 +1,25 @@
+export const videoParams = {
+  // mediasoup params
+  encodings: [
+    {
+      rid: "r0",
+      maxBitrate: 100000,
+      scalabilityMode: "S1T3",
+    },
+    {
+      rid: "r1",
+      maxBitrate: 300000,
+      scalabilityMode: "S1T3",
+    },
+    {
+      rid: "r2",
+      maxBitrate: 900000,
+      scalabilityMode: "S1T3",
+    },
+  ],
+  codecOptions: {
+    videoGoogleStartBitrate: 1000,
+  },
+};
+
+export const MAX_SHARE_SCREEN_SIZE = 4;
