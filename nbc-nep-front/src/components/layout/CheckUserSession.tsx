2b56940ba046697edea8b3e3e17e6006f514272f
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-01-24 21:49:11 +0900

    [fix] : supabase 오류 나던거 수정...

--- nbc-nep-front/src/components/layout/CheckUserSession.tsx
+++ nbc-nep-front/src/components/layout/CheckUserSession.tsx
@@ -7,16 +7,34 @@ export default function CheckUserSession() {
   const { logout, login } = useAuth();
 
   useEffect(() => {
-    supabase.auth.onAuthStateChange(async (event, session) => {
+    const channel = supabase.auth.onAuthStateChange((event, session) => {
+      console.log(event);
       if (event === "INITIAL_SESSION" || event === "SIGNED_IN") {
         if (!session) return;
-        const user = await getUserSessionHandler(session);
-        login(user);
+        getUserSessionHandler(session)
+          .then((user) => {
+            login(user);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+
+        // 여기가 실행이 안됨
       } else if (event === "SIGNED_OUT") {
         logout();
       }
     });
+
+    return () => {
+      channel.data.subscription.unsubscribe();
+    };
   }, []);
 
   return <></>;
 }
+
+export const getServerSideProps = async () => {
+  return {
+    props: {},
+  };
+};
