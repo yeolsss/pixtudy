ab0a1bd3111cb346ca41d8d2b5e494af5614e524
Kim MyungSub <audtjqxx@naver.com>
2024-01-25 15:34:19 +0900

    [fix]: 유효하지 않은 메타버스 접근시 dashboard로 리다이렉션

--- nbc-nep-front/src/middleware.ts
+++ nbc-nep-front/src/middleware.ts
@@ -20,6 +20,16 @@ export async function middleware(request: NextRequest) {
 
   const { pathname } = request.nextUrl;
 
+  // supabase에 접근하여 유효한 spaceid인지 확인
+  const checkSpace = async (id: string) => {
+    const { data: spaceInfo } = await supabase
+      .from("spaces")
+      .select("*")
+      .eq("id", id)
+      .single();
+    return spaceInfo ? true : false;
+  };
+
   // 정적 파일, 이미지(public 포함), 프리패칭에 대한 요청을 허용하는 로직
   const isPublicResource =
     pathname.startsWith("/assets/") || pathname.startsWith("/styles/");
@@ -37,7 +47,7 @@ export async function middleware(request: NextRequest) {
 
   // request 정보가 지정한 path에 등록된 정보인지 확인
   const isDynamicPath = PAGES_PATH.some(
-    (page) => page.dynamic && pathname.startsWith(`${page.path}`)
+    (page) => page.dynamic && pathname.startsWith(`${page.path}/`)
   );
   const isStaticPath = PAGES_PATH.some(
     (page) => !page.dynamic && pathname === page.path
@@ -52,7 +62,7 @@ export async function middleware(request: NextRequest) {
   // 로그인 세션에 따른 조건부 처리
   if (
     !session &&
-    (pathname.startsWith("/dashboard") || pathname.startsWith("/metaverse"))
+    (pathname.startsWith("/dashboard") || pathname.startsWith("/metaverse/"))
   ) {
     console.log("세션이 없는데 dashboard, metaverse에 들어옴");
     return NextResponse.redirect(new URL("/signin", request.url));
@@ -64,6 +74,16 @@ export async function middleware(request: NextRequest) {
   ) {
     console.log("세션은 있는데 signin singup 페이지에 들어옴");
     return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  // 유효한 메타버스 id가 없을 때
+  if (session && pathname.startsWith("/metaverse")) {
+    const spaceId = request.url.split("=").at(-1);
+    await checkSpace(spaceId!);
+    const checkResult = await checkSpace(spaceId!);
+    if (!checkResult) {
+      return NextResponse.redirect(new URL("/dashboard", request.url));
+    }
   }
 
   return response;
