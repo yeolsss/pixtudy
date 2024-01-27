76e95c7c3efd9ecd14eb506168c6c3e3ba672372
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-01-27 19:13:29 +0900

    [add] : supabase upload thumbnail 로직 추가

---
+++ nbc-nep-front/src/api/supabase/storage.ts
@@ -0,0 +1,17 @@
+/* Storage */
+
+import { supabase } from "@/supabase/supabase";
+
+export const uploadThumbnail = async (
+  thumbnailFile: Blob | File,
+  fileName: string
+) => {
+  const BASE_URL =
+    "https://lrklhpcxbdiunpubmvio.supabase.co/storage/v1/object/public/space_thumbnails/";
+  const BUCKET_NAME = "space_thumbnails";
+
+  const { data, error } = await supabase.storage
+    .from(BUCKET_NAME)
+    .upload(fileName, thumbnailFile);
+  return { data: BASE_URL + data?.path, error };
+};
