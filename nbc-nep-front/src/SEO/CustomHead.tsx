72b2daef4a50134b9d953cc6b1d02dd3fa983c84
초짜맨 <yongseung.song@icloud.com>
2024-01-29 13:25:39 +0900

    [update] styling 수정

--- nbc-nep-front/src/SEO/CustomHead.tsx
+++ nbc-nep-front/src/SEO/CustomHead.tsx
@@ -9,6 +9,18 @@ export default function CustomHead({ title, description }: Props) {
     <Head>
       <title>{`Pixtudy | ${title}`}</title>
       <meta name="description" content={description} />
+      {/* Open Graph tags */}
+      <meta
+        property="og:title"
+        content={`Pixtudy | 메타버스 공간에서 함께해요!`}
+      />
+      <meta
+        property="og:description"
+        content={`Pixtudy는 메타버스 환경에서 화면, 카메라 및 음성을 공유하고 스크럼보드를 이용해 협업할 수 있는 웹 애플리케이션입니다.`}
+      />
+      <meta property="og:url" content="https://www.pixtudy.site" />
+      <meta property="og:image" content="../assets/auth/authHero5.png" />
+
       <link
         href="https://cdn.jsdelivr.net/npm/galmuri/dist/galmuri.css"
         rel="stylesheet"
