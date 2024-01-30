1a2f3be620ddef684370bfe5942189dc8999d389
초짜맨 <yongseung.song@icloud.com>
2024-01-31 02:23:34 +0900

    [add] 스크롤 애니메이션 적용중

--- nbc-nep-front/src/components/introduction/PixelBackground.tsx
+++ nbc-nep-front/src/components/introduction/PixelBackground.tsx
@@ -14,11 +14,11 @@ const anim: (i: number) => Variants = (i: number) => {
     },
     open: (i) => ({
       opacity: 1,
-      transition: { duration: 0, delay: 0.03 * i },
+      transition: { duration: 0, delay: 0.02 * i },
     }),
     closed: (i) => ({
       opacity: 0,
-      transition: { duration: 0, delay: 0.03 * i },
+      transition: { duration: 0, delay: 0.02 * i },
     }),
   };
 };
@@ -47,7 +47,7 @@ export default function PixelBackground({ isInSection, position }: Props) {
 
   const getBlocks = () => {
     if (!innerHeight || !innerWidth) return;
-    const blockSize = innerWidth * 0.05;
+    const blockSize = innerWidth * 0.02;
     const numberOfBlocks = Math.ceil(innerHeight / blockSize);
     const shuffledArray = shuffle(
       [...Array(numberOfBlocks)].map((_, index) => index)
@@ -81,10 +81,10 @@ const StPixelBackground = styled.div<{
   flex-direction: column;
   width: 100vw;
   height: 100vh;
-  top: ${(props) => props.$position.top}px;
+  top: 0;
   left: 0;
   overflow: hidden;
-  position: absolute;
+  position: fixed;
   z-index: 0;
   pointer-events: none;
 
@@ -96,7 +96,8 @@ const StPixelBackground = styled.div<{
     & > div {
       width: 10vw;
       height: 100%;
-      background-color: #ff6a00;
+      /* background-color: #ff6a00; */
+      background-color: ${(props) => props.theme.color.bg.brand};
     }
   }
 `;
--- nbc-nep-front/src/pages/index.tsx
+++ nbc-nep-front/src/pages/index.tsx
@@ -31,12 +31,12 @@ const Home: NextPageWithLayout = () => {
   // }, []);
 
   useEffect(() => {
-    const { top, left } = wrapperRef.current?.getBoundingClientRect()!;
+    const { top, left, bottom } = wrapperRef.current?.getBoundingClientRect()!;
     setPosition({ top, left });
     const handleScroll = () => {
       const { scrollY } = window;
       console.log(scrollY);
-      if (scrollY > top) {
+      if (scrollY > top && scrollY < bottom) {
         setIsInSection(true);
       } else {
         setIsInSection(false);
@@ -76,54 +76,62 @@ const Home: NextPageWithLayout = () => {
               인기 랜선스터디 모아보기 {">"}
             </StLink>
           </StHeroBannerWrapper>
-        </StHeroBanner>
-        <StSection>
           <Image
-            src="/assets/introduction/section1.png"
-            alt="hero image"
-            width={479}
-            height={430}
+            src={"/assets/introduction/landing.png"}
+            width={658}
+            height={563}
+            alt="come and study together!"
           />
-          <StSectionContents>
-            <p>
-              <span>Pixtudy</span>는 혁신적인 가상 업무 및 스터디 공간을
-              제공하여 뛰어난 협업 경험을 제공할 수 있습니다. 여기서는 단순한
-              업무 전달을 넘어, 팀원들과의 화상 교류를 통해 새로운 아이디어를
-              탐색하고, 프로젝트를 좀 더 생동감 있게 진행할 수 있는 환경을
-              마련해 드립니다.
-            </p>
-            <p>
-              여러개의 화면을 공유하고, 공유한 화면을 입맛에 맞게 배치해 화면을
-              구성하고, 스크럼 보드에서 프로젝트 완성을 위한 업무를 계획하고
-              분배하는 등 가상환경 내에서 자유롭게 소통하고 협업해보세요. 이
-              과정에서 생겨나는 아이디어는 팀의 창의력을 자극하고, 업무의
-              효율성을 높여줄 겁니다.
-            </p>
-            <p>
-              <span>Pixtudy</span>에서 아이디어가 살아 숨 쉬는 공간을
-              경험해보세요!
-            </p>
-          </StSectionContents>
-        </StSection>
-        <StPixelBackground isInSection={isInSection} position={position} />
-        <StSection ref={wrapperRef}>
-          <video src="/assets/introduction/walk.mp4" autoPlay muted loop />
-          <StSectionContents>
-            <h2>Pixtudy에서의 회의는 다릅니다.</h2>
-            <p>
-              화상/음성을 통한 유저간 실시간 소통과 최대 4개의 화면을 동시에
-              공유할 수 있는 커스텀 레이아웃을 통해 완벽한 회의를 진행하세요
-            </p>
-          </StSectionContents>
-        </StSection>
-        <StSection>
-          <video
-            src="/assets/introduction/mediashare.mp4"
-            autoPlay
-            muted
-            loop
-          />
-        </StSection>
+        </StHeroBanner>
+        <StSectionWrapper ref={wrapperRef}>
+          <StSection $isInSection={isInSection}>
+            <Image
+              src="/assets/introduction/section1.png"
+              alt="hero image"
+              width={479}
+              height={430}
+            />
+            <StSectionContents>
+              <p>
+                <span>Pixtudy</span>는 혁신적인 가상 업무 및 스터디 공간으로
+                뛰어난 협업 경험을 제공합니다. 단순한 업무 전달을 넘어,
+                팀원들과의 화상 교류를 통해 새로운 아이디어를 탐색하고,
+                프로젝트를 좀 더 생동감 있게 진행해 보세요.
+              </p>
+              <p>
+                여러개의 화면을 공유하고, 공유한 화면을 입맛에 맞게 배치해
+                화면을 구성하고, 스크럼 보드에서 프로젝트 완성을 위한 업무를
+                계획하고 분배하는 등 가상환경 내에서 자유롭게 소통하고
+                협업해보세요. 이 과정에서 생겨나는 아이디어는 팀의 창의력을
+                자극하고, 업무의 효율성을 높여줄 겁니다.
+              </p>
+              <p>
+                <span>Pixtudy</span>에서 아이디어가 살아 숨 쉬는 공간을
+                경험해보세요!
+              </p>
+            </StSectionContents>
+          </StSection>
+          <StPixelBackground isInSection={isInSection} position={position} />
+          <StSection $isInSection={isInSection}>
+            <video src="/assets/introduction/walk.mp4" autoPlay muted loop />
+            <StSectionContents>
+              <h2>Pixtudy에서의 회의는 다릅니다.</h2>
+              <p>
+                화상/음성을 통한 유저간 실시간 소통과 최대 4개의 화면을 동시에
+                공유할 수 있는 커스텀 레이아웃을 통해 완벽한 회의를 진행하세요
+              </p>
+            </StSectionContents>
+          </StSection>
+          <StSection $isInSection={isInSection}>
+            <video
+              src="/assets/introduction/mediashare.mp4"
+              autoPlay
+              muted
+              loop
+            />
+          </StSection>
+        </StSectionWrapper>
+        <div style={{ height: "2000px" }}>mo?lu</div>
       </StWrapper>
     </>
   );
@@ -144,18 +152,20 @@ const StWrapper = styled.div`
 `;
 
 const StHeroBanner = styled.div`
+  display: flex;
+  justify-content: center;
   position: relative;
   max-width: 1200px;
   width: 100%;
   height: 100%;
   padding: ${(props) => props.theme.spacing[40]};
-
-  margin: 0 auto;
-
-  background-image: url("./assets/introduction/landing.png");
-  background-repeat: no-repeat;
-  background-size: 50%;
-  background-position: 100% 0;
+  z-index: 2;
+  margin: ${(props) => props.theme.spacing[80]} auto;
+  & > img {
+    width: 658px;
+    height: 563px;
+    object-fit: cover;
+  }
 `;
 
 const StHeroBannerWrapper = styled.div`
@@ -202,18 +212,30 @@ const StLink = styled(StCTALink)`
   font-size: ${(props) => props.theme.body.md.medium.fontSize};
 `;
 
-const StSection = styled.div`
+const StSectionWrapper = styled.div`
   position: relative;
+  max-width: 1200px;
+  width: 100%;
+  height: 3600px;
+  margin: 0 auto;
+`;
+
+const StSection = styled.div<{ $isInSection: boolean }>`
+  /* position: relative; */
+  position: sticky;
+  top: calc(50vh - ${HEIGHT}px);
+  left: 0;
   max-width: 1200px;
   margin: 0 auto;
   width: 100%;
-  height: 100%;
+  height: 1200px;
   padding: ${(props) => props.theme.spacing[40]};
-
   display: flex;
   align-items: center;
   justify-content: center;
-
+  z-index: 2;
+  opacity: ${(props) => (props.$isInSection ? 1 : 0)};
+  transition: opacity 0.2s ease-in-out;
   gap: ${(props) => props.theme.spacing[24]};
 
   & > video {
@@ -240,7 +262,7 @@ export const StSectionContents = styled.div`
     font-size: ${(props) => props.theme.body.lg.regular.fontSize};
     line-height: ${(props) => props.theme.body.lg.regular.lineHeight};
     letter-spacing: ${(props) => props.theme.body.lg.regular.letterSpacing};
-
+    word-break: keep-all;
     & > span {
       font-family: var(--point-font);
       font-weight: bold;
