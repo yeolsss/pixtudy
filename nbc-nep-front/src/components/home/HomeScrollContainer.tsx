7525816a085b54f720d86a92342a5e2f7831f800
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-01-31 12:13:55 +0900

    [style] homepage 스크롤 애니메이션 추가

---
+++ nbc-nep-front/src/components/home/HomeScrollContainer.tsx
@@ -0,0 +1,48 @@
+import { AnimatePresence } from "framer-motion";
+import { useEffect, useState } from "react";
+import ScrollItem from "./ScrollItem";
+import {
+  StScrollSection,
+  StStickyItem,
+  StStickyWrapper,
+} from "./styles/home.styles";
+const SCROLL_THRESHOLDS = [2160, 3160, 4160, 5160];
+
+export default function HomeScrollContainer() {
+  const [index, setIndex] = useState(-1);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY < SCROLL_THRESHOLDS[0]) {
+        setIndex(-1);
+        return;
+      }
+      SCROLL_THRESHOLDS.forEach((v, i) => {
+        if (window.scrollY > v) setIndex(i);
+      });
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <StScrollSection>
+      <StStickyWrapper>
+        <StStickyItem>
+          <h1>Pixtudy에서의 회의는 다릅니다.</h1>
+          <p>
+            화상/음성을 통한 유저간 실시간 소통과 최대 4개의 화면을 동시에
+            공유할 수 있는 커스텀 레이아웃을 통해 완벽한 회의를 진행하세요
+          </p>
+        </StStickyItem>
+        <AnimatePresence>
+          {Array.from({ length: index + 1 }).map((_, i) => (
+            <ScrollItem key={`scroll-item${i}`} index={i} />
+          ))}
+        </AnimatePresence>
+      </StStickyWrapper>
+    </StScrollSection>
+  );
+}
---
+++ nbc-nep-front/src/components/home/styles/home.styles.tsx
@@ -0,0 +1,105 @@
+import { motion } from "framer-motion";
+import styled from "styled-components";
+
+export const StScrollSection = styled.section`
+  width: 100vw;
+  height: 500vh;
+
+  display: flex;
+  flex-direction: row;
+`;
+
+export const StStickyWrapper = styled.div`
+  position: sticky;
+  width: 1350px;
+  margin: 0 auto;
+  top: ${(props) => props.theme.spacing[16]};
+  height: 100vh;
+
+  background: url("/assets/home/sticky-bg.png") no-repeat 0 35% / contain;
+  padding: ${(props) =>
+    `${props.theme.spacing[16]} ${props.theme.spacing[32]}`};
+`;
+
+export const StStickyItem = styled.div`
+  position: relative;
+
+  width: 29rem;
+
+  top: 40%;
+  transform: translateY(-50%);
+  left: ${(props) => props.theme.spacing[32]};
+
+  * {
+    font-family: var(--default-font);
+    word-break: keep-all;
+  }
+  display: flex;
+  flex-direction: column;
+  gap: ${(props) => props.theme.spacing[16]};
+
+  h1 {
+    font-size: ${(props) => props.theme.heading.desktop["3xl"].fontSize};
+    letter-spacing: ${(props) =>
+      props.theme.heading.desktop["3xl"].letterSpacing};
+    line-height: ${(props) => props.theme.heading.desktop["3xl"].lineHeight};
+  }
+
+  p {
+    font-size: ${(props) => props.theme.body.md.medium.fontSize};
+    letter-spacing: ${(props) => props.theme.body.md.medium.letterSpacing};
+    line-height: ${(props) => props.theme.body.md.medium.lineHeight};
+  }
+`;
+
+export const StScrollItemWrapper = styled(motion.div)<{
+  $top: string;
+  $left: string;
+}>`
+  position: absolute;
+  top: ${(props) => props.$top};
+  left: ${(props) => props.$left};
+
+  width: 30rem;
+  height: 38rem;
+
+  display: flex;
+  padding: ${(props) => props.theme.spacing[32]};
+  flex-direction: column;
+  align-items: center;
+  gap: ${(props) => props.theme.spacing[16]};
+
+  background-color: #fff;
+  border: 1px solid ${(props) => props.theme.color.bg.secondary};
+  box-shadow: ${(props) => props.theme.elevation.Light.shadow2};
+  border-radius: ${(props) => props.theme.border.radius[16]};
+
+  div {
+    flex: 4;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: ${(props) => props.theme.spacing[32]};
+
+    h2 {
+      max-width: 60%;
+      font-size: ${(props) => props.theme.heading.desktop["sm"].fontSize};
+      letter-spacing: ${(props) =>
+        props.theme.heading.desktop["sm"].letterSpacing};
+      line-height: ${(props) => props.theme.heading.desktop["sm"].lineHeight};
+      font-family: ${(props) => props.theme.heading.desktop["sm"].fontFamily};
+      text-align: center;
+      word-break: keep-all;
+    }
+  }
+
+  p {
+    flex: 1;
+    font-size: ${(props) => props.theme.body.md.medium.fontSize};
+    letter-spacing: ${(props) => props.theme.body.md.medium.letterSpacing};
+    line-height: ${(props) => props.theme.body.md.medium.lineHeight};
+    text-align: center;
+    word-break: keep-all;
+  }
+`;
