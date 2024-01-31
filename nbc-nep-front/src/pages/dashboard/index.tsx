8d718b3155bf5b0dc480565febfe41454c266821
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-01-31 14:11:17 +0900

    [update] : ssr -> ssg 전환

--- nbc-nep-front/src/pages/dashboard/index.tsx
+++ nbc-nep-front/src/pages/dashboard/index.tsx
@@ -120,7 +120,7 @@ Dashboard.getLayout = function getLayout(page: ReactElement) {
   return <Layout>{page}</Layout>;
 };
 
-export const getServerSideProps = async () => {
+export const getStaticProps = async () => {
   const supabase = createClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
