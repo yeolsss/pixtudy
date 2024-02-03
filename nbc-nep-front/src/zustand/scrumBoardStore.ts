2b335319946fe3a0f8533830e9559a2eeb84b000
yeolsss <yeol8810@gmail.com>
2024-02-03 14:49:27 +0900

    [fix] useScrumBoardStore에 관한 zustand 변경

--- nbc-nep-front/src/zustand/scrumBoardStore.ts
+++ nbc-nep-front/src/zustand/scrumBoardStore.ts
@@ -1,5 +1,6 @@
 import { Kanban_categories } from "@/supabase/types/supabase.tables.type";
 import { create } from "zustand";
+import createSelectors from "@/zustand/config/createSelector";
 
 interface ScrumBoardState {
   categories: Kanban_categories[];
@@ -10,11 +11,12 @@ const initialState = {
   categories: [],
 };
 
-const useScrumBoardStore = create<ScrumBoardState>((set) => ({
+const scrumBoardStore = create<ScrumBoardState>((set) => ({
   ...initialState,
   setCategories: (fetchedCategories: Kanban_categories[]) => {
     set({ categories: fetchedCategories });
   },
 }));
 
+const useScrumBoardStore = createSelectors(scrumBoardStore);
 export default useScrumBoardStore;
