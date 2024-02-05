563e50b6a1f2a1600db804c513728feaacdea6b4
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-02-05 16:56:38 +0900

    [add] : eslint 적용하면서 사라졌던 내용 추가

--- nbc-nep-front/src/components/common/SpaceThumb.tsx
+++ nbc-nep-front/src/components/common/SpaceThumb.tsx
@@ -7,6 +7,13 @@ interface Props {
   src: string | undefined;
 }
 
+const StImage = styled(Image)`
+  border-radius: ${(props) => props.theme.border.radius[8]};
+  padding: 0;
+  margin: 0;
+  object-fit: cover;
+`;
+
 export default function SpaceThumb({
   width = 250,
   height = 150,
